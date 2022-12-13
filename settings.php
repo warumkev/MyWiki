c736fc34cff98ddee0dfb9e871fb5ad2aa455688
Kev <k.tame2003@gmail.com>
2022-12-13 20:16:15 +0100

    Worked on Settings Panel

--- settings.php
+++ settings.php
@@ -2,6 +2,27 @@
 session_start();
 include('./includes/connect.php');
 
+if (!isset($_SESSION['isAdmin'])) {
+  header("Location: home.php");
+}
+
+if (isset($_GET["table"])) {
+
+  $table = $_GET["table"];
+
+  if (strcmp($table, "users") == 0 || strcmp($table, "posts") == 0) {
+
+    pg_query($dbConn, "TRUNCATE public.$table RESTART IDENTITY CASCADE;");
+
+    if (strcmp($table, "users") == 0) {
+
+      header('Location: logout.php');
+
+    } else {
+      header('Location: settings.php');
+    }
+  }
+}
 ?>
 
 <!doctype html>
@@ -12,8 +33,7 @@ include('./includes/connect.php');
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>MyWiki | Startseite</title>
   <link rel="icon" type="image/x-icon" href="assets/brand/wikiLogo.svg">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 
 <body>
@@ -42,16 +62,14 @@ include('./includes/connect.php');
 
     ?>
     <p class="fs-2"><code>public.users</code></p>
-    <button type="button" class="btn btn-primary position-relative" data-bs-toggle="modal"
-      data-bs-target="#staticBackdrop">
+    <button type="button" class="btn btn-primary position-relative" data-bs-toggle="modal" data-bs-target="#trunUsers">
       Zurücksetzen
       <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
         <?php echo pg_num_rows($usersResult); ?>
       </span>
     </button>
 
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-      aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal fade" id="trunUsers" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
@@ -63,8 +81,10 @@ include('./includes/connect.php');
             machen möchtest?
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Abbrechen</button>
-            <button type="button" class="btn btn-primary">Ja</button>
+            <form method="post" action="settings.php?table=users">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Abbrechen</button>
+              <button type="post" class="btn btn-primary" name="trunUsers" id="trunUsers">Ja</button>
+            </form>
           </div>
         </div>
       </div>
@@ -74,22 +94,16 @@ include('./includes/connect.php');
 
     $postsResult = pg_query($dbConn, "SELECT * FROM public.posts");
 
-    if (isset($_POST['trunPosts'])) {
-      echo "Hi";
-    }
-
     ?>
     <p class="fs-2"><code>public.posts</code></p>
-    <button type="button" class="btn btn-primary position-relative" data-bs-toggle="modal"
-      data-bs-target="#staticBackdrop">
+    <button type="button" class="btn btn-primary position-relative" data-bs-toggle="modal" data-bs-target="#trunPosts">
       Zurücksetzen
       <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
         <?php echo pg_num_rows($postsResult); ?>
       </span>
     </button>
 
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-      aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal fade" id="trunPosts" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
@@ -102,20 +116,14 @@ include('./includes/connect.php');
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Abbrechen</button>
-            <a href="" type="button" class="btn btn-primary">Ja</a>
+            <a href="settings.php?table=posts" class="btn btn-primary" name="trunPosts" id="trunPosts">Ja</a>
           </div>
         </div>
       </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-      crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-      integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-      crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js"
-      integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk"
-      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
 </body>
 
 </html>
