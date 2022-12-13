3a1c7219890ddee58281eac873664df0d12e6f07
kev9euf3rois <118884361+kev9euf3rois@users.noreply.github.com>
2022-12-13 20:38:57 +0100

    Added "isAdmin" in public.users

--- databaseDump.sql
+++ databaseDump.sql
@@ -92,12 +92,24 @@ ALTER SEQUENCE public.posts_id_seq OWNED BY public.posts.id;
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.users (
-    id integer NOT NULL,
-    username text NOT NULL,
-    passwordhash text NOT NULL,
-    email text NOT NULL
-);
+-- Table: public.users
+
+-- DROP TABLE IF EXISTS public.users;
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+    username text COLLATE pg_catalog."default" NOT NULL,
+    passwordhash text COLLATE pg_catalog."default" NOT NULL,
+    email text COLLATE pg_catalog."default" NOT NULL,
+    "isAdminAccount" boolean NOT NULL DEFAULT false,
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.users
+    OWNER to postgres;
 
 
 ALTER TABLE public.users OWNER TO postgres;
