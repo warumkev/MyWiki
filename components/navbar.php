69031eabddde4545e3e78aa73adf0949cbf05ed5
kev9euf3rois <k.tamme2003@gmail.com>
2022-12-18 13:37:53 +0100

    Translated into english

--- components/navbar.php
+++ components/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg bg-light ">
   <div class="container-fluid">
     <a class="navbar-brand" href="home.php"><img src="./assets/brand/wikiLogo.svg" width="30" height="30"
         class="d-inline-block align-top" alt=""> <span class="text-warning">my</span>Wiki</a>
@@ -10,22 +10,22 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="home.php">Startseite</a>
+          <a class="nav-link active" aria-current="page" href="home.php">Homepage</a>
         </li>
         <?php if (!isset($_SESSION['loggedin'])) { ?>
         <li class="nav-item">
-          <a class="nav-link active" href="login.php">Anmelden</a>
+          <a class="nav-link active" href="login.php">Log in</a>
         </li>
         <?php } else { ?>
         <li class="nav-item">
-          <a class="nav-link active" href="create.php">Beitrag erstellen</a>
+          <a class="nav-link active" href="create.php">Create Post</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="account.php?authorid=<?php echo $_SESSION['userid']; ?>">Mein Account</a>
+          <a class="nav-link active" href="account.php?authorid=<?php echo $_SESSION['userid']; ?>">My account</a>
         </li>
         <?php if (isset($_SESSION['isAdmin'])) { ?>
         <li class="nav-item">
-          <a class="nav-link active" href="settings.php">Einstellungen</a>
+          <a class="nav-link active" href="settings.php">Settings</a>
         </li>
         <?php } ?>
         <?php } ?>
@@ -33,13 +33,13 @@
           <a class="nav-link active" href="faq.php">FAQ</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="post.php?id=<?php echo $randomPost; ?>">Entdecken</a>
+          <a class="nav-link active" href="post.php?id=<?php echo $randomPost; ?>">Discover</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
-        <input class="form-control me-2 btn btn-outline-warning" type="search" placeholder="Search" name="search"
+        <input class="form-control me-2 btn btn-outline-dark" type="search" placeholder="Search our database" name="search"
           aria-label="Search">
-        <input class="btn btn-outline-dark" type="submit">
+        <input class="btn btn-outline-warning" type="submit" value="Search">
       </form>
     </div>
   </div>
